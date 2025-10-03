0f0206f38c8e6b440b67e77fc52d66db0fb8635c
Douglas G. Moore <doug@39alpharesearch.org>
2025-10-03 15:05:59 -0700

    :art: Rename Iₘᵢₙ as Imin in WilliamsBeer payload

--- src/williamsbeer.jl
+++ src/williamsbeer.jl
@@ -1,23 +1,23 @@
 using Printf
 
 mutable struct WilliamsBeer
-    Iₘᵢₙ::Float64
+    Imin::Float64
     Π::Float64
 end
 Base.zero(::Type{WilliamsBeer}) = WilliamsBeer(0.0, 0.0)
 
 function Base.isequal(p::WilliamsBeer, q::WilliamsBeer)
-    isequal(p.Iₘᵢₙ, q.Iₘᵢₙ) && isequal(p.Π, q.Π)
+    isequal(p.Imin, q.Imin) && isequal(p.Π, q.Π)
 end
 
-Base.:(==)(p::WilliamsBeer, q::WilliamsBeer) = p.Iₘᵢₙ == q.Iₘᵢₙ && p.Π == q.Π
+Base.:(==)(p::WilliamsBeer, q::WilliamsBeer) = p.Imin == q.Imin && p.Π == q.Π
 
-Base.:≈(p::WilliamsBeer, q::WilliamsBeer) = p.Iₘᵢₙ ≈ q.Iₘᵢₙ && p.Π ≈ q.Π
+Base.:≈(p::WilliamsBeer, q::WilliamsBeer) = p.Imin ≈ q.Imin && p.Π ≈ q.Π
 
 Base.iszero(p::WilliamsBeer) = p.Π == zero(p.Π)
 
 function Base.show(io::IO, ::MIME"text/dot", p::WilliamsBeer)
-    @printf io "Π = %0.3f\\nIₘᵢₙ = %0.3f" p.Π p.Iₘᵢₙ
+    @printf io "Π = %0.3f\\nImin = %0.3f" p.Π p.Imin
 end
 
 function pid!(h::Hasse{<:AbstractVertex{WilliamsBeer}},
@@ -46,9 +46,9 @@ function pid!(h::Hasse{<:AbstractVertex{WilliamsBeer}},
             for k in 2:length(α)
                 x = min(x, si[α[k]][s])
             end
-            payload(α).Iₘᵢₙ += sdist[s] * x;
+            payload(α).Imin += sdist[s] * x;
         end
-        payload(α).Iₘᵢₙ /= sdist.N
+        payload(α).Imin /= sdist.N
     end
 
     for i in eachindex(h)
@@ -67,30 +67,30 @@ function pid!(h::Hasse{<:AbstractVertex{WilliamsBeer}},
             end
             payload(α).Π += sdist[s] * u
         end
-        payload(α).Π = payload(α).Iₘᵢₙ - payload(α).Π / sdist.N
+        payload(α).Π = payload(α).Imin - payload(α).Π / sdist.N
     end
 
     h
 end
 
 mutable struct ExactWilliamsBeer
-    Iₘᵢₙ::ExactResult
+    Imin::ExactResult
     Π::ExactResult
 end
 Base.zero(::Type{ExactWilliamsBeer}) = ExactWilliamsBeer(zero(ExactResult), zero(ExactResult))
 
 function Base.isequal(p::ExactWilliamsBeer, q::ExactWilliamsBeer)
-    isequal(p.Iₘᵢₙ, q.Iₘᵢₙ) && isequal(p.Π, q.Π)
+    isequal(p.Imin, q.Imin) && isequal(p.Π, q.Π)
 end
 
-Base.:(==)(p::ExactWilliamsBeer, q::ExactWilliamsBeer) = p.Iₘᵢₙ == q.Iₘᵢₙ && p.Π == q.Π
+Base.:(==)(p::ExactWilliamsBeer, q::ExactWilliamsBeer) = p.Imin == q.Imin && p.Π == q.Π
 
-Base.:≈(p::ExactWilliamsBeer, q::ExactWilliamsBeer) = p.Iₘᵢₙ ≈ q.Iₘᵢₙ && p.Π ≈ q.Π
+Base.:≈(p::ExactWilliamsBeer, q::ExactWilliamsBeer) = p.Imin ≈ q.Imin && p.Π ≈ q.Π
 
 Base.iszero(p::ExactWilliamsBeer) = p.Π == zero(p.Π)
 
 function Base.show(io::IO, ::MIME"text/dot", p::ExactWilliamsBeer)
-    @printf io "Π = %s\\nIₘᵢₙ = %s" p.Π p.Iₘᵢₙ
+    @printf io "Π = %s\\nImin = %s" p.Π p.Imin
 end
 
 function pid!(h::Hasse{<:AbstractVertex{ExactWilliamsBeer}},
@@ -119,9 +119,9 @@ function pid!(h::Hasse{<:AbstractVertex{ExactWilliamsBeer}},
             for k in 2:length(α)
                 x = min(x, si[α[k]][s])
             end
-            payload(α).Iₘᵢₙ += sdist[s] * x;
+            payload(α).Imin += sdist[s] * x;
         end
-        payload(α).Iₘᵢₙ /= sdist.N;
+        payload(α).Imin /= sdist.N;
     end
 
     for i in eachindex(h)
@@ -139,7 +139,7 @@ function pid!(h::Hasse{<:AbstractVertex{ExactWilliamsBeer}},
                 payload(α).Π += sdist[s] * maximum(us)
             end
         end
-        payload(α).Π = payload(α).Iₘᵢₙ - payload(α).Π / sdist.N
+        payload(α).Π = payload(α).Imin - payload(α).Π / sdist.N
     end
 
     h
